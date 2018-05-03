7bd7f9c9941d9f972ec590ef2a186810eacc5cb4
GitDarren <darren321@gmail.com>
2018-05-02 21:31:29 -0500

    Changing website over to GatsbyJS. Initital Build process.

---
+++ darren-portfolio/src/pages/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum in
+          magnam harum! Dolorem in illum, nemo quam, laudantium aperiam rem ad
+          natus, architecto alias eaque libero culpa facilis accusantium.
+          Consequatur ut ipsa tempore accusantium sint quisquam facilis numquam
+          nemo laudantium!
+        </p>
+      </div>
+    )
+  }
+}
